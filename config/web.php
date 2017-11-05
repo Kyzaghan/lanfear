0dfcb0c55d7a78ec0fd755bbf0d22fbd8d1fa1ec
Kyzaghan <kyzaghan@ismailkose.com.tr>
2017-11-05 15:46:28 +0300

    Giriş ekranında ki bug giderildi.

--- config/web.php
+++ config/web.php
@@ -24,7 +24,7 @@ $config = [
             'class' => 'yii\caching\FileCache',
         ],
         'user' => [
-            'identityClass' => 'app\models\Users',
+            'identityClass' => 'app\models\database\Users',
             'enableAutoLogin' => true,
         ],
         'errorHandler' => [
